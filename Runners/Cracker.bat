46a9362e1035cb8806f143c1ec14ea126f50c094
Matin Abedini <96107976+harsurcraft@users.noreply.github.com>
2023-02-10 00:47:23 -0800

    Delete Runners directory

--- Runners/Cracker.bat
+++
@@ -1,4 +0,0 @@
-msg * Hi, to run this tool you need too install Python and some times because of wordlist it didnt work you can use defult wordlist: Harsurcraft (Thanks a lot form yasan and MR.tabari an another teackers.)
-
-cd code
-python .\main.py
--- Runners/Cracker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Hi, to run this tool you need too install Python and some times because of wordlist it didnt work you can use defult wordlist: Harsurcraft (Thanks a lot form yasan and MR.tabari an another teackers.
-echo 
-echo ___________________________________________________________________________________________________________________________________________________________________________________________________________________
-echo
-
-cd code
-python main.py
